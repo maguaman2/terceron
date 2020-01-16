3a3729de470633a641acf8cb7a7aa2f25d57596a
maguaman2 <59964049+maguaman2@users.noreply.github.com>
2020-01-16 17:03:14 -0500

    Formato para estudiantes

--- ejercicio1.sql
+++ ejercicio1.sql
@@ -1,7 +1,7 @@
 /*Creacion de base de datos*/
 /*profesor*/
-
-
+create database sysacad;
+use sysacad;
 /*Alumno1*/
 
 
@@ -11,3 +11,30 @@
 
 
 /*Alumno3*/
+
+
+/*Alumno4*/
+
+
+/*Alumno5*/
+
+
+/*Alumno6*/
+
+
+/*Alumno7*/
+
+
+/*Alumno8*/
+
+
+/*Alumno9*/
+
+
+/*Alumno10*/
+
+
+/*Alumno11*/
+
+
+/*Alumno12*/
